e226388eb385d4254fbd7fc03c8aca5b94b09ad1
HANZO-YT <100697890+HANZO-YT@users.noreply.github.com>
2023-04-10 18:14:06 +0600

    Create urls.py

---
+++ users/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+
+from users.views import LoginCBV, LogoutCBV, RegisterCBV
+
+urlpatterns = [
+    path("login/", LoginCBV.as_view()),
+    path("logout/", LogoutCBV.as_view()),
+    path("register/", RegisterCBV.as_view()),
+]
