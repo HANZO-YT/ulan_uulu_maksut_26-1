9b0d48e2e3607b13342c88b87d15621c928f8748
HANZO-YT <100697890+HANZO-YT@users.noreply.github.com>
2023-04-10 18:16:05 +0600

    hw8

---
+++ products/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+
+from products.views import CreateProductCBV, DateNowCBV, GoodbyeCBV, HelloCBV, MainCBV, ProductDetailCBV, ProductsCBV
+
+urlpatterns = [
+    path("products/", ProductsCBV.as_view()),
+    path("", MainCBV.as_view()),
+    path("hello/", HelloCBV.as_view()),
+    path("goodbye/", GoodbyeCBV.as_view()),
+    path("products/<int:id>/", ProductDetailCBV.as_view()),
+    path("products/create/", CreateProductCBV.as_view()),
+    path("now_date/", DateNowCBV.as_view()),
+]
